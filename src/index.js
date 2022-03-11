3a8645152c6e2b11f401b8eb4b7c60ad5253e452
Natália Barbosa <nob.ufba@gmail.com>
2022-03-11 14:07:24 -0300

    paletas de cores e rascunho da página de login para servir de base

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
-import './index.scss';
+import './styles/index.scss';
+
 import { StoresProvider } from './stores';
 
 ReactDOM.render(
