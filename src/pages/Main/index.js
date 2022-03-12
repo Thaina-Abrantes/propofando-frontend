cc9088dddf99db0af7c75dbab971f01756c9932f
Natália Barbosa <nob.ufba@gmail.com>
2022-03-12 18:17:45 -0300

    Modal excluir, os modais ainda não possuem lógica

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,14 @@
 import Header from 'components/Header';
-import Modal from 'components/Modal';
+// import Modal from 'components/Modal';
+import ModalDelete from 'components/ModalDelete';
 import style from './styles.module.scss';
 
 export function Main() {
   return (
     <div className={style['container-main']}>
       <Header />
-      <Modal title="Editar Dados" button="Salvar alterações" />
+      {/* <Modal title="Editar Dados" button="Salvar alterações" /> */}
+      <ModalDelete title="categoria" />
       <h1>Hello World</h1>
       <h1>Footer</h1>
     </div>
