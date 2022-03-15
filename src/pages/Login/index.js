871328b19350fd0e9eca12184507b6dde046cdf4
Natália Barbosa <nob.ufba@gmail.com>
2022-03-15 19:19:50 -0300

    página de login com validação yup. Pequenos ajustes no css. Está autenticando sem validar

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,79 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { loginSchema } from 'validations/loginValidation';
 import LogoDark from '../../assets/logo-dark.svg';
 import style from './styles.module.scss';
 
+const defaultValues = { email: '', senha: '' };
+
 export function Login() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState(defaultValues);
+  const [erroEmail, setErroEmail] = useState('');
+  const [erroPassword, setErroPassword] = useState('');
+
+  async function handleSubmit(event) {
+    setErroEmail('');
+    setErroPassword('');
+    event.preventDefault();
+
+    try {
+      await loginSchema.validateSync({
+        email: event.target[0].value,
+        password: event.target[1].value,
+      });
+    } catch (err) {
+      if (err.params.path === 'email') {
+        setErroEmail(err.message);
+      }
+      if (err.params.path === 'password') {
+        setErroPassword(err.message);
+      }
+    }
+  }
+
+  function handleChange(target) {
+    setForm({ ...form, [target.name]: target.value });
+  }
+
   return (
     <div className={style.container}>
 
       <div className={style.containerCard}>
         <img src={LogoDark} alt="Logo Propofando" />
 
-        <form>
-          <span>Faça seu Login</span>
-          <input className="input" type="text" placeholder="email" />
-          <input className="input" placeholder="senha" type="password" />
+        <form onSubmit={handleSubmit}>
+          <h2>Faça seu Login</h2>
+          <div className="containerInput">
+            <input
+              name="email"
+              className={erroEmail ? 'error-dark' : 'input'}
+              value={form.email}
+              type="text"
+              placeholder="email"
+              onChange={(e) => { handleChange(e.target); }}
+            />
+            {erroEmail && <span className="error-message ">{erroEmail}</span>}
+          </div>
+          <div className="containerInput">
+            <input
+              name="senha"
+              className={erroPassword ? 'error-dark' : 'input'}
+              value={form.senha}
+              placeholder="senha"
+              type="password"
+              onChange={(e) => { handleChange(e.target); }}
+            />
+            {erroPassword && <span className="error-message ">{erroPassword}</span>}
+          </div>
           <div>
-            <button className="button">Entrar</button>
+            <button
+              // onClick={() => navigate('/main')}
+              className="button"
+            >
+              Entrar
+
+            </button>
           </div>
         </form>
       </div>
