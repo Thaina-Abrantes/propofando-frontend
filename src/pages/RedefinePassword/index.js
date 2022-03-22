497fb4f9e1a50f0768b242e35f9a08834178fb9c
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-03-22 14:56:03 -0300

    cria estados e adiciona 'div' successfully

--- src/pages/RedefinePassword/index.js
+++ src/pages/RedefinePassword/index.js
@@ -1,15 +1,35 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import style from './styles.module.scss';
 import logo from '../../assets/logo-dark.svg';
 import arrow from '../../assets/arrow-back-icon-white.svg';
+import icon from '../../assets/confirmation-icon.svg';
 
 export function RedefinePassword() {
+  const navigate = useNavigate();
+  const [resetSuccessfully, setResetSuccessfully] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setResetSuccessfully(true);
+  }
+
+  function loginBack() {
+    return (
+      navigate('/')
+    );
+  }
+
   return (
     <div className={style['container-main']}>
       <div className={style['container-imgs']}>
-        <img className={style['arrow-img']} src={arrow} alt="Seta voltar" />
+        <a className={style['arrow-img']} onClick={loginBack}>
+          <img src={arrow} alt="Seta voltar" />
+        </a>
         <img src={logo} alt="Logo" />
       </div>
-      <div className={style['container-card']}>
+      {!resetSuccessfully && (
+      <form className={style['container-card']} onSubmit={handleSubmit}>
         <h1>Redefinir senha</h1>
         <p>
           Preencha os campos abaixo para definir uma
@@ -31,7 +51,22 @@ export function RedefinePassword() {
         <button className="button">
           Confirmar
         </button>
-      </div>
+      </form>
+      )}
+      {resetSuccessfully && (
+        <div className={style['container-successfully']}>
+          <img className={style['icon']} src={icon} alt="Ícone de confirmação" />
+          <h1>Senha redefinida com sucesso!</h1>
+          <p>
+            Use a nova senha cadastrada para realizar o
+            <br />
+            login na plataforma.
+          </p>
+          <button className="button" onClick={loginBack}>
+            Voltar para login
+          </button>
+        </div>
+      )}
     </div>
   );
 }
