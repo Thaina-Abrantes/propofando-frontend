973872d3287ebd8967485ce57fb20f0c082cc3e9
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-03-22 18:17:06 -0300

    validação de senha

--- src/pages/RedefinePassword/index.js
+++ src/pages/RedefinePassword/index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { newPasswordSchema } from 'validations/redefineValidation';
 import style from './styles.module.scss';
 import logo from '../../assets/logo-dark.svg';
 import arrow from '../../assets/arrow-back-icon-white.svg';
@@ -8,10 +9,35 @@ import icon from '../../assets/confirmation-icon.svg';
 export function RedefinePassword() {
   const navigate = useNavigate();
   const [resetSuccessfully, setResetSuccessfully] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [erroConfirmPassword, setErroConfirmPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [erroNewPassword, setErroNewPassword] = useState('');
 
   async function handleSubmit(e) {
     e.preventDefault();
-    setResetSuccessfully(true);
+
+    try {
+      newPasswordSchema.validateSync({
+        password: e.target[0].value,
+        passwordConfirmation: e.target[1].value,
+      });
+    } catch (err) {
+      if (err.params.path === 'password') {
+        setErroNewPassword(err.message);
+      }
+      if (err.params.path === 'passwordConfirmation') {
+        setErroConfirmPassword(err.message);
+      }
+    }
+  }
+
+  function handleChangeInput1(value) {
+    setNewPassword(value);
+  }
+
+  function handleChangeInput2(value) {
+    setConfirmPassword(value);
   }
 
   function loginBack() {
@@ -29,29 +55,33 @@ export function RedefinePassword() {
         <img src={logo} alt="Logo" />
       </div>
       {!resetSuccessfully && (
-      <form className={style['container-card']} onSubmit={handleSubmit}>
-        <h1>Redefinir senha</h1>
-        <p>
-          Preencha os campos abaixo para definir uma
-          <br />
-          nova senha
-        </p>
-        <input
-          className="input"
-          name="password"
-          type="text"
-          placeholder="Nova senha"
-        />
-        <input
-          className="input"
-          name="password"
-          type="text"
-          placeholder="Confirmar senha"
-        />
-        <button className="button">
-          Confirmar
-        </button>
-      </form>
+        <form className={style['container-card']} onSubmit={handleSubmit}>
+          <h1>Redefinir senha</h1>
+          <p>
+            Preencha os campos abaixo para definir uma
+            <br />
+            nova senha
+          </p>
+          <input
+            className={erroNewPassword ? 'error-dark' : 'input'}
+            name="password"
+            type="text"
+            placeholder="Nova senha"
+            onChange={(e) => { handleChangeInput1(e.target.value); }}
+          />
+          {erroNewPassword && <span className="error-message ">{erroNewPassword}</span>}
+          <input
+            className={erroConfirmPassword ? 'error-dark' : 'input'}
+            name="password"
+            type="text"
+            placeholder="Confirmar senha"
+            onChange={(e) => { handleChangeInput2(e.target.value); }}
+          />
+          {erroConfirmPassword && <span className="error-message ">{erroConfirmPassword}</span>}
+          <button className="button">
+            Confirmar
+          </button>
+        </form>
       )}
       {resetSuccessfully && (
         <div className={style['container-successfully']}>
--- src/validations/redefineValidation.js
+++ src/validations/redefineValidation.js
@@ -1,7 +1,12 @@
 import * as yup from 'yup';
 
-const redefineSchema = yup.object().shape({
-  email: yup.string().required('Email é obrigatório').email('Favor informar um email válido'),
+const emailSchema = yup.object().shape({
+  email: yup.string().required('Email é obrigatório.').email('Favor informar um email válido.'),
 });
 
-export { redefineSchema };
+const newPasswordSchema = yup.object().shape({
+  password: yup.string().required('O campo nova senha é obrigatório.').min(8, 'Mínimo de 8 caracteres.'),
+  passwordConfirmation: yup.string().required('O campo confirmar senha é obrigatório.').oneOf([yup.ref('password'), null], 'As senhas devem ser iguais.'),
+});
+
+export { emailSchema, newPasswordSchema };
