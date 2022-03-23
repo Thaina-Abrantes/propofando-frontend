800d25582bfdc4758ec10f2b0eaca3ad73b843ff
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-03-23 14:43:44 -0300

    header dark mode da pag alunos

---
+++ src/components/HeaderAluno/index.js
@@ -0,0 +1,16 @@
+import style from './styles.module.scss';
+import logo from '../../assets/logo-dark.svg';
+import userImg from '../../assets/accoutn-circle.svg';
+
+export default function HeaderAluno() {
+  return (
+    <header className={style['logo-container']}>
+      <img src={logo} alt="Logo" />
+      <div className={style['user-container']}>
+        <span>Usuário</span>
+        <img src={userImg} alt="Usuário" />
+      </div>
+
+    </header>
+  );
+}
