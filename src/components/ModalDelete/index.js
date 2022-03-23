c99e0531c4fbd0d0450ee12a1a5d626763fc69b5
Natália Barbosa <nob.ufba@gmail.com>
2022-03-23 11:11:30 -0300

    Modal deletar, posição dos elementos

--- src/components/ModalDelete/index.js
+++ src/components/ModalDelete/index.js
@@ -3,21 +3,23 @@ import clear from '../../assets/clear-icon.svg';
 
 function ModalDelete({ title }) {
   return (
-    <div className={style.container}>
-      <img src={clear} alt="Close" />
-      <div>
-        <h2>
-          Excluir
-          {' '}
-          {title}
-        </h2>
-        <p>
-          Tem certeza que deseja excluir esse
-          {' '}
-          {title}
-          ? Essa ação não poderá ser desfeita.
-        </p>
-        <button className="button">Excluir</button>
+    <div className={style.background}>
+      <div className={style.container}>
+        <button><img src={clear} alt="Close" /></button>
+        <div>
+          <h2>
+            Excluir
+            {' '}
+            {title}
+          </h2>
+          <p>
+            Tem certeza que deseja excluir esse
+            {' '}
+            {title}
+            ? Essa ação não poderá ser desfeita.
+          </p>
+          <button className="button">Excluir</button>
+        </div>
       </div>
     </div>
   );
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import Header from 'components/Header';
+import ModalDelete from 'components/ModalDelete';
 
 import style from './styles.module.scss';
 
@@ -6,7 +7,7 @@ export function Main() {
   return (
     <div className={style['container-main']}>
       <Header />
-
+      <ModalDelete />
     </div>
   );
 }
