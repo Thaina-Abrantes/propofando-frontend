935930dd7fbf4376a78d2126c6d3d2b6b2458572
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-03-24 15:39:12 -0300

    botões da pagina

--- src/pages/StudentPage/index.js
+++ src/pages/StudentPage/index.js
@@ -2,6 +2,11 @@ import style from './styles.module.scss';
 
 export function StudentPage() {
   return (
-    <div />
+    <div className={style['container-page']}>
+      <div className={style['container-buttons']}>
+        <button className="button">Criar um simulado</button>
+        <button className="button">Meus simulados</button>
+      </div>
+    </div>
   );
 }
