5bbacff2b72b2207cf3f35b1e2becbc12ca08226
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-03-24 14:54:17 -0300

    adiciona o componente StudentPanel e a pág. Studentpage

--- src/pages/StudentMain/index.js
+++ src/pages/StudentMain/index.js
@@ -1,4 +1,6 @@
 import StudentHeader from 'components/StudentHeader';
+import StudentPanel from 'components/StudentPanel';
+import { Outlet } from 'react-router-dom';
 
 import style from './styles.module.scss';
 
@@ -6,7 +8,12 @@ export function StudentMain() {
   return (
     <div className={style['container-main']}>
       <StudentHeader />
-
+      <div className={style['div-wrapper']}>
+        <StudentPanel />
+        <div className={style['div-center']}>
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
