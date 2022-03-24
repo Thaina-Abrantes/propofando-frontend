eb1875e9703b80870663639e125b492dca2ee072
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-03-24 16:27:41 -0300

    estrutura dos cards

--- src/pages/StudentPage/index.js
+++ src/pages/StudentPage/index.js
@@ -7,6 +7,17 @@ export function StudentPage() {
         <button className="button">Criar um simulado</button>
         <button className="button">Meus simulados</button>
       </div>
+      <h2>Desempenho</h2>
+      <div className={style['container-cards']}>
+        <div className={style['card']}>
+          <h3>Simulados feitos</h3>
+          <h1>5</h1>
+        </div>
+        <div className={style['card']}>
+          <h3>Questões respondidas</h3>
+          <h1>20%</h1>
+        </div>
+      </div>
     </div>
   );
 }
