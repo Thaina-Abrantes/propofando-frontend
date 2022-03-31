7c640e402ae3fa91a92a931d8b3ebae59177ec59
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-03-31 16:56:10 -0300

    construindo o styles da pág

--- src/pages/Simulated/index.js
+++ src/pages/Simulated/index.js
@@ -8,12 +8,12 @@ export function Simulated() {
     <main className={style['container-main']}>
       <StudentHeader />
 
-      <div className={style['container']}>
-        <div className={style['container-title']}>
-          <img src={arrow} alt="Seta" />
-          <h1>Simulado</h1>
-        </div>
+      <div className={style['container-title']}>
+        <img src={arrow} alt="Seta" />
+        <h1>Simulado</h1>
+      </div>
 
+      <div className={style.container}>
         <form>
           <div className={style['container-question']}>
             <h1 className={style['question-title']}>
@@ -31,20 +31,24 @@ export function Simulated() {
             <div className={style['question-alternatives']}>
               <div className={style['alternative']}>
                 <input type="radio" id="a" />
+                <span>A) Repicar o curare.</span>
               </div>
-              <div className={style['alternative']}>
+              <div className={style.alternative}>
                 <input type="radio" id="b" />
+                <span>B) Verificar a pressão do balonete.</span>
               </div>
-              <div className={style['alternative']}>
+              <div className={style.alternative}>
                 <input type="radio" id="c" />
+                <span>C) Verificar a conexão da linha de capnografia.</span>
               </div>
-              <div className={style['alternative']}>
+              <div className={style.alternative}>
                 <input type="radio" id="d" />
+                <span>D) Trocar a cal sodada.</span>
               </div>
             </div>
           </div>
 
-          <div className={style['container-buttons']} />
+          <div className={style.buttons} />
         </form>
       </div>
     </main>
