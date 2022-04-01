42875427e517bcd491461c3dbda4d78e851509f2
Natália Barbosa <nob.ufba@gmail.com>
2022-04-01 13:18:36 -0300

    Elementos principais já posicionados

--- src/pages/MySimulated/index.js
+++ src/pages/MySimulated/index.js
@@ -1,6 +1,7 @@
 import StudentHeader from 'components/StudentHeader';
 import style from './styles.module.scss';
 import seta from '../../assets/arrow-back-icon.svg';
+import factCheck from '../../assets/fact-check-icon.svg';
 
 export function MySimulated() {
   return (
@@ -17,6 +18,34 @@ export function MySimulated() {
             <option value="0">Todos</option>
           </select>
         </label>
+      </div>
+
+      <div className={style['table-body']}>
+        <div className={style['table-line']}>
+          <div className={style['first-item']}>
+            <img src={factCheck} alt="listar" />
+            <span>05/03/2022</span>
+          </div>
+          <div className={style['second-item']}>
+            <span>Simulado 1</span>
+          </div>
+          <div className={style['third-item']}>
+            <button className="button">Consultar respostas</button>
+          </div>
+        </div>
+        <div className={style['table-line']}>
+          <div className={style['first-item']}>
+            <img src={factCheck} alt="listar" />
+            <span>03/03/2022</span>
+          </div>
+          <div className={style['second-item']}>
+            <span>Simulado 2</span>
+          </div>
+          <div className={style['third-item']}>
+            <button className="button">Consultar respostas</button>
+          </div>
+        </div>
+
       </div>
     </main>
   );
