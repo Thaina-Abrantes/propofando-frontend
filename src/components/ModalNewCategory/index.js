03db2be0eb7ab3b58bec46170735fa2d735e9a13
Natália Barbosa <nob.ufba@gmail.com>
2022-04-05 20:27:47 -0300

    logica para enviar a categoria para o banco. sem autorização ainda

--- src/components/ModalNewCategory/index.js
+++ src/components/ModalNewCategory/index.js
@@ -1,14 +1,41 @@
 import { useStores } from 'stores';
+import { useState } from 'react';
 import style from './styles.module.scss';
 import clear from '../../assets/clear-icon.svg';
 
 function ModalNewCategory() {
+  const [category, setCategory] = useState('');
+
   const {
     modalStore: {
       openModalNewCategory,
       setOpenModalNewCategory,
     },
   } = useStores();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  async function handleRegisterCategory() {
+    const body = {
+      name: category,
+    };
+    if (!category);
+
+    try {
+      fetch('https://localhost:3333/category', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Authorization: `Bearer ${auth.token}`,
+        },
+        body: JSON.stringify(body),
+      });
+    } catch (error) {
+      return error.message;
+    }
+  }
+
   return (
     <div className={style.background}>
       <div className={style.container}>
@@ -19,16 +46,28 @@ function ModalNewCategory() {
             <img src={clear} alt="Close" />
           </button>
         </div>
-        <div>
+        <form onSubmit={handleSubmit}>
           <h2>Nova categoria</h2>
 
           <label>
             Título
           </label>
-          <input className="input-light" placeholder="Categoria" />
+          <input
+            className="input-light"
+            placeholder="Categoria"
+            name="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
 
-          <button className="button">Adicionar categoria</button>
-        </div>
+          <button
+            onClick={() => handleRegisterCategory()}
+            className="button"
+          >
+            Adicionar categoria
+
+          </button>
+        </form>
       </div>
     </div>
   );
