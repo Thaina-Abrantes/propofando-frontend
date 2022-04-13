fc9666166ddcd0f5df7b640f8688da8da5bc087f
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-04-13 11:21:39 -0300

    fetch das questões

--- src/pages/Questions/index.js
+++ src/pages/Questions/index.js
@@ -1,10 +1,35 @@
 import SearchQuestion from 'components/SearchQuestion';
+import { useEffect, useState } from 'react';
 import style from './styles.module.scss';
 import pasteIcon from '../../assets/content-paste-icon.svg';
 import editIcon from '../../assets/edit-icon.svg';
 import deleteIcon from '../../assets/delete-icon.svg';
+import api from '../../services/api';
 
 export function Questions() {
+  const [allQuestions, setAllQuestions] = useState([]);
+
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzMTNmNTdhLTJiMzQtNDU0Yi04ZTJlLTEyOGQ2NDllNGJkOSIsImVtYWlsIjoibWFudUBlbWFpbC5jb20iLCJ1c2VyVHlwZSI6InN1cGVyIGFkbWluIiwiaWF0IjoxNjQ5ODUzMTUwLCJleHAiOjE2NDk5Mzk1NTB9.GsJlgvXfrvf1dirj4Hkm6jvhte-Pvqg8h8cPGlIRUOw';
+
+  useEffect(() => {
+    handleFilterQuestion();
+  }, [allQuestions]);
+
+  async function handleFilterQuestion() {
+    try {
+      const response = await api.get('/questions', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const { data } = response;
+      setAllQuestions(data.questions);
+      console.log(allQuestions);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <main>
       <SearchQuestion />
@@ -19,18 +44,6 @@ export function Questions() {
             <div className={style['first-line-item']}>
               <img src={pasteIcon} alt="pasta" />
               <span>VUNESP - EsFCEx - Oficial Médico </span>
-            </div>
-            <div className={style['second-line-item']}>
-              <button>
-                <img src={editIcon} alt="editar" />
-              </button>
-              <button><img src={deleteIcon} alt="deletar" /></button>
-            </div>
-          </div>
-          <div className={style['table-line']}>
-            <div className={style['first-line-item']}>
-              <img src={pasteIcon} alt="pasta" />
-              <span>FUNDATEC - GHC - Médico </span>
             </div>
             <div className={style['second-line-item']}>
               <button>
