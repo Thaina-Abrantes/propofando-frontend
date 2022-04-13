2859a1d54d025412b5b237cec02de12f8b70bb24
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-04-13 10:03:12 -0300

    filtrar categorias

--- src/components/SearchCategory/index.js
+++ src/components/SearchCategory/index.js
@@ -3,13 +3,18 @@ import style from './styles.module.scss';
 import arrowBack from '../../assets/arrow-back-icon.svg';
 import searchIcon from '../../assets/search-icon.svg';
 
-function SearchCategory() {
+function SearchCategory({ setSearchItem }) {
   const {
     modalStore: {
       openModalNewCategory,
       setOpenModalNewCategory,
     },
   } = useStores();
+
+  function handleChange(e) {
+    setSearchItem(e.target.value);
+  }
+
   return (
     <div className={style.search}>
       <div>
@@ -18,7 +23,11 @@ function SearchCategory() {
       </div>
       <div className={style['search-container']}>
         <img src={searchIcon} alt="Lupa" />
-        <input className="input-light" placeholder="Pesquisar" />
+        <input
+          className="input-light"
+          placeholder="Pesquisar"
+          onChange={handleChange}
+        />
         <button
           onClick={() => setOpenModalNewCategory(true)}
           className="button"
--- src/pages/QuestionCategory/index.js
+++ src/pages/QuestionCategory/index.js
@@ -1,13 +1,37 @@
 import SearchCategory from 'components/SearchCategory';
 import { useNavigate } from 'react-router-dom';
 import { useStores } from 'stores';
+import { useEffect, useState } from 'react';
 import style from './styles.module.scss';
 import topicIcon from '../../assets/topic-icon.svg';
 import editIcon from '../../assets/edit-icon.svg';
 import deleteIcon from '../../assets/delete-icon.svg';
+import api from '../../services/api';
 
 export function QuestionCategory() {
   const navigate = useNavigate();
+  const [serchItem, setSearchItem] = useState('');
+  const [allCategories, setAllCategories] = useState([]);
+
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzMTNmNTdhLTJiMzQtNDU0Yi04ZTJlLTEyOGQ2NDllNGJkOSIsImVtYWlsIjoibWFudUBlbWFpbC5jb20iLCJ1c2VyVHlwZSI6InN1cGVyIGFkbWluIiwiaWF0IjoxNjQ5NzkyOTkzLCJleHAiOjE2NDk4NzkzOTN9.vaAeQmJW0MvrqlOs47l6rqogh-jraZUR9qior1pS15E';
+
+  useEffect(() => {
+    handleFilterCategory();
+  }, [allCategories]);
+
+  async function handleFilterCategory() {
+    try {
+      const response = await api.get('/categories', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const { data } = response;
+      setAllCategories(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const {
     modalStore: {
@@ -18,7 +42,7 @@ export function QuestionCategory() {
 
   return (
     <main>
-      <SearchCategory />
+      <SearchCategory setSearchTerm={setSearchItem} />
       <div className={style['table']}>
         <div className={style['table-header']}>
           <div className={style['name-title']}><span>Nome</span></div>
@@ -26,38 +50,26 @@ export function QuestionCategory() {
           <div className={style['manage-title']}><span>Gerenciar</span></div>
         </div>
         <div className={style['table-body']}>
-          <div className={style['table-line']}>
-            <div className={style['first-line-item']} onClick={() => navigate('/main/list-question')}>
-              <img src={topicIcon} alt="Categoria" />
-              <span>Categoria A</span>
-            </div>
-            <div className={style['second-line-item']}>
-              <span>30</span>
-            </div>
+          {allCategories.filter((item) => item.name.toLocaleLowerCase()
+            .includes(serchItem.toLocaleLowerCase()))
+            .map((item) => (
+              <div className={style['table-line']} key={item.id}>
+                <div className={style['first-line-item']} onClick={() => navigate('/main/list-question')}>
+                  <img src={topicIcon} alt="Categoria" />
+                  <span>{item.name}</span>
+                </div>
+                <div className={style['second-line-item']}>
+                  <span>{item.totalQuestions}</span>
+                </div>
 
-            <div className={style['third-line-item']}>
-              <button>
-                <img src={editIcon} alt="editar" />
-              </button>
-              <button onClick={() => setOpenModalDeleteCategory(true)}><img src={deleteIcon} alt="deletar" /></button>
-            </div>
-          </div>
-          <div className={style['table-line']}>
-            <div className={style['first-line-item']} onClick={() => navigate('/main/list-question')}>
-              <img src={topicIcon} alt="Categoria" />
-              <span>Categoria B</span>
-            </div>
-            <div className={style['second-line-item']}>
-              <span>40</span>
-            </div>
-
-            <div className={style['third-line-item']}>
-              <button>
-                <img src={editIcon} alt="editar" />
-              </button>
-              <button onClick={() => setOpenModalDeleteCategory(true)}><img src={deleteIcon} alt="deletar" /></button>
-            </div>
-          </div>
+                <div className={style['third-line-item']}>
+                  <button>
+                    <img src={editIcon} alt="editar" />
+                  </button>
+                  <button onClick={() => setOpenModalDeleteCategory(true)}><img src={deleteIcon} alt="deletar" /></button>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </main>
