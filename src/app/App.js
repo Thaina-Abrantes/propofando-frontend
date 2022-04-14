b41c64aa3ca103187832dcf44cff71e93ec71e35
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-04-14 19:57:53 -0300

    timeout alert

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import CustomAlert from 'components/CustomAlert';
-import { Suspense, useState } from 'react';
+import { Suspense, useEffect } from 'react';
 import { useStores } from 'stores';
 import { MyRoutes } from './routes';
 import styles from './styles.module.scss';
@@ -7,6 +7,14 @@ import styles from './styles.module.scss';
 function App() {
   const { utilsStore: { alert, setAlert } } = useStores();
   const { open, type, message } = alert;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert({ open: false });
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [open]);
 
   return (
     <Suspense fallback={null}>
