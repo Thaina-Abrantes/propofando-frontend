8c6bd847e9d580fff74afe1b62a5452b32d9664f
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-04-14 21:49:12 -0300

    icon close alert

--- src/components/CustomAlert/index.js
+++ src/components/CustomAlert/index.js
@@ -1,20 +1,39 @@
+import { Collapse, IconButton, SvgIcon } from '@mui/material';
 import Alert from '@mui/material/Alert';
+import { useStores } from 'stores';
 import styles from './styles.module.scss';
+import { ReactComponent as CloseIcon } from '../../assets/close-icon.svg';
 
 export default function CustomAlert({ open, message, type }) {
+  const { utilsStore: { setAlert } } = useStores();
   return (
     <>
       {' '}
       {open
-
         && (
-          <Alert
-            variant="standard"
-            severity={type}
-            className={`${styles['container-alert']} ${styles[type || 'error']}`}
-          >
-            {message}
-          </Alert>
+          <Collapse in={open}>
+            <Alert
+              variant="standard"
+              severity={type}
+              className={`${styles['container-alert']} ${styles[type || 'error']}`}
+              action={(
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => {
+                    setAlert({ open: false });
+                  }}
+                >
+                  <SvgIcon className={styles['teste']}>
+                    <CloseIcon />
+                  </SvgIcon>
+                </IconButton>
+              )}
+            >
+              {message}
+            </Alert>
+          </Collapse>
         )}
     </>
   );
