388fba20e596ee88872e3d9d8b7ee56d047f4986
Natália Barbosa <nob.ufba@gmail.com>
2022-04-16 22:42:19 -0300

    select com seta já clicavél e estilos. Falta adicionar como css global

--- src/pages/AddQuestion/index.js
+++ src/pages/AddQuestion/index.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import clip from '../../assets/annex-icon.svg';
 import arrowBack from '../../assets/arrow-back-icon.svg';
-import arrowDown from '../../assets/arrow-down.svg';
+import arrowDown from '../../assets/arrow.svg';
 import style from './styles.module.scss';
 
 const defaultValuesForm = {
@@ -126,6 +126,7 @@ export function AddQuestion() {
                 Categoria
                 <select>
                   <option value="0">Select</option>
+                  <option value="id da categoria">Nome da Categoria</option>
                 </select>
               </label>
               <img src={arrowDown} alt="seta" />
