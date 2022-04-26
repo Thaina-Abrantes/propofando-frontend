04f06cc257f05b7d043162a5b758840786b9697b
Natália Barbosa <nob.ufba@gmail.com>
2022-04-26 13:38:17 -0300

    Header puxando os nomes do usuários e do admin

--- src/components/StudentHeader/index.js
+++ src/components/StudentHeader/index.js
@@ -1,13 +1,20 @@
+import { useStores } from 'stores';
 import style from './styles.module.scss';
 import logo from '../../assets/logo-dark.svg';
 import userImg from '../../assets/accoutn-circle.svg';
 
 export default function StudentHeader() {
+  const {
+    userStore: {
+      userData,
+    },
+  } = useStores();
+  const currentName = userData.name.split(' ');
   return (
     <header className={style['header-container']}>
       <img src={logo} alt="Logo" />
       <div className={style['user-container']}>
-        <span>Usuário</span>
+        <span>{currentName[0]}</span>
         <img src={userImg} alt="Usuário" />
       </div>
     </header>
