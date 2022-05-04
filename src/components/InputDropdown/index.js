f43d00fc2eba3ab42644d31861dd3cc1fa73d827
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-05-04 13:46:28 -0300

    seta os itens checked no array de selectedOptions

--- src/components/InputDropdown/index.js
+++ src/components/InputDropdown/index.js
@@ -5,22 +5,40 @@ import arrowUp from '../../assets/arrow-up.svg';
 
 export function InputDropdown({ list }) {
   const [openDropdown, setOpenDropdown] = useState(false);
-  const [selectedOption, setSelectedOption] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
-  function handleSelect(item) {
-    if (selectedOption.includes(item.name)) {
-      return;
+  // function handleSelect() {
+  //   if (selectedOptions.includes(item.name)) {
+  //     return;
+  //   }
+  //   setSelectedOptions([...selectedOptions, item.name]);
+  // }
+
+  function handleChange(target) {
+    if (target.checked) {
+      setSelectedOptions([...selectedOptions, target.name]);
+    } else {
+      const filtered = selectedOptions.filter((i) => i !== target.name);
+      setSelectedOptions(filtered);
     }
-    setSelectedOption([...selectedOption, item.name]);
-    console.log(selectedOption);
   }
+
+  function handleChangeAll(target) {
+    if (target.checked) {
+      const everything = list.map((i) => i.name);
+      setSelectedOptions(everything);
+    } else {
+      setSelectedOptions([]);
+    }
+  }
+  console.log(selectedOptions);
 
   return (
     <div className={style['container-input']}>
       <div className={style.dropdown}>
         <div className={openDropdown ? style['dropdown-select'] : style['dropdown-select-closed']} onClick={() => setOpenDropdown(!openDropdown)}>
-          {selectedOption.length > 0
-            ? <div className={style['selected-items']}><span>{[...selectedOption.join(', ')]}</span></div>
+          {selectedOptions.length > 0
+            ? <div className={style['selected-items']}><span>{[...selectedOptions.join(', ')]}</span></div>
             : <span>Selecionar</span>}
           <img src={openDropdown ? arrowUp : arrowDown} alt="Seta" />
         </div>
@@ -32,18 +50,20 @@ export function InputDropdown({ list }) {
                   type="checkbox"
                   id="Todas"
                   name="Todas"
+                  onChange={(e) => handleChangeAll(e.target)}
                 />
                 <span className={style.checkmark} />
                 Todas
               </label>
             </div>
             {list.map((item) => (
-              <div key={item.id} onClick={() => handleSelect(item)}>
+              <div key={item.id}>
                 <label className={style['dropdown-option']}>
                   <input
                     type="checkbox"
                     id={item.id}
                     name={item.name}
+                    onChange={(e) => handleChange(e.target)}
                   />
                   <span className={style.checkmark} />
                   {item.name}
