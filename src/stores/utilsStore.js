886c4b5009d76e94e0133893b152d1b283ca3dd8
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-05-17 18:16:07 -0300

    remove estado desnecessário

--- src/stores/utilsStore.js
+++ src/stores/utilsStore.js
@@ -4,7 +4,6 @@ export function useUtils() {
   const [openReportProblem, setOpenReportProblem] = useState(false);
   const [openExplanation, setOpenExplanation] = useState(false);
   const [openQuestionStatistics, setOpenQuestionStatistics] = useState(false);
-  const [pagesTests, setPagesTests] = useState('');
   const [alert, setAlert] = useState({
     open: false,
   });
@@ -45,8 +44,6 @@ export function useUtils() {
       handleOpenQuestionStatistics,
       handleOpenReportProblem,
       handleCloseBox,
-      pagesTests,
-      setPagesTests,
     }
   );
 }
