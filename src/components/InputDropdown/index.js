1c90814b7acf9aeec10aa29af0057d7860b3c6b9
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-05-20 00:44:29 -0300

    criando simulado conforme categorias selecionadas

--- src/components/InputDropdown/index.js
+++ src/components/InputDropdown/index.js
@@ -1,43 +1,65 @@
 import { useState } from 'react';
-import { useStores } from 'stores';
 import style from './styles.module.scss';
 import triangleDown from '../../assets/triangle-down.svg';
 import triangleUp from '../../assets/triangle-up.svg';
 
-export function InputDropdown({ list }) {
+export function InputDropdown({ list, categoriesIds, setCategoriesIds }) {
   const [openDropdown, setOpenDropdown] = useState(false);
   const [selectedOptions, setSelectedOptions] = useState([]);
   const [allSelected, setAllSelected] = useState(false);
 
-  function handleChange(target) {
-    if (target.checked && target.name === 'Todas') {
+  function handleChange({ name, id, checked }) {
+    if (checked && name === 'Todas') {
       setAllSelected(true);
+
       const everything = list.map((i) => i.name);
       setSelectedOptions(everything);
-    } else if (!target.checked && target.name === 'Todas') {
+
+      const everythingIds = list.map((i) => i.id);
+      setCategoriesIds(everythingIds);
+    } else if (!checked && name === 'Todas') {
       setAllSelected(false);
       setSelectedOptions([]);
-    } else if (target.checked && target.name !== 'Todas') {
-      if (selectedOptions.includes(target.name)) {
+      setCategoriesIds([]);
+    } else if (checked && name !== 'Todas') {
+      if (selectedOptions.includes(name)) {
         return;
       }
-      setSelectedOptions([...selectedOptions, target.name]);
+      setSelectedOptions([...selectedOptions, name]);
+      setCategoriesIds([...categoriesIds, id]);
     } else {
       if (allSelected) {
         return;
       }
-      const filtered = selectedOptions.filter((i) => i !== target.name);
+      const filtered = selectedOptions.filter((i) => i !== name);
       setSelectedOptions(filtered);
+
+      const filteredIds = categoriesIds.filter((i) => i !== id);
+      setCategoriesIds(filteredIds);
     }
   }
 
   return (
     <div className={style['container-input']}>
-      <div className={openDropdown ? style['dropdown-select'] : style['dropdown-select-closed']} onClick={() => setOpenDropdown(!openDropdown)}>
+      <div
+        className={openDropdown
+          ? style['dropdown-select']
+          : style['dropdown-select-closed']}
+        onClick={() => setOpenDropdown(!openDropdown)}
+      >
         {selectedOptions.length > 0
-          ? <div className={style['selected-items']}><span>{[...selectedOptions.join(', ')]}</span></div>
+          ? (
+            <div className={style['selected-items']}>
+              <span>{[...selectedOptions.join(', ')]}</span>
+            </div>
+          )
           : <span>Selecionar</span>}
-        <img src={openDropdown ? triangleUp : triangleDown} alt="Seta" />
+        <img
+          src={openDropdown
+            ? triangleUp
+            : triangleDown}
+          alt="Seta"
+        />
       </div>
       {openDropdown && (
         <div className={style['dropdown-options']}>
